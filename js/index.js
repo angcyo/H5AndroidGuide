12ba114b7a106f17d9c0e14fa835789d94d48825
angcyo <angcyo@126.com>
2019-04-11 14:33:20 +0800

    add csdn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("Hello Index.js")
+//console.log("Hello Index.js")
+
+
+fill.loading('.jcodecraeer')
+http.get("http://127.0.0.1:9898/jcodecraeer", {
+    onData: (data) => {
+        fill.fill(".jcodecraeer", data)
+    }
+});
+
+fill.loading('.angcyo')
+http.get("http://127.0.0.1:9898/angcyo", {
+    onData: (data) => {
+        fill.fill(".angcyo", data)
+    }
+});
