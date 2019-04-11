2714c6db2926c7a075b33a96b8f0deed916199e6
angcyo <angcyo@126.com>
2019-04-11 16:16:30 +0800

    add scrollerbar css

--- js/index.js
+++ js/index.js
@@ -1,43 +1,44 @@
 //console.log("Hello Index.js")
 
+const BASE_URL = 'http://127.0.0.1:9898'
 
 fill.loading('.jcodecraeer')
-http.get("http://127.0.0.1:9898/jcodecraeer", {
+http.get(`${BASE_URL}/jcodecraeer`, {
     onData: (data) => {
         fill.fill(".jcodecraeer", data)
     }
 });
 
 fill.loading('.angcyo')
-http.get("http://127.0.0.1:9898/angcyo", {
+http.get(`${BASE_URL}/angcyo`, {
     onData: (data) => {
         fill.fill(".angcyo", data)
     }
 });
 
 fill.loading('.ithome')
-http.get("http://127.0.0.1:9898/ithome", {
+http.get(`${BASE_URL}/ithome`, {
     onData: (data) => {
         fill.fill(".ithome", data)
     }
 });
 
 fill.loading('.androidweekly')
-http.get("http://127.0.0.1:9898/androidweekly", {
+http.get(`${BASE_URL}/androidweekly`, {
     onData: (data) => {
         fill.fill(".androidweekly", data)
     }
 });
 
 fill.loading('.wanandroid')
-http.get("http://127.0.0.1:9898/wanandroid", {
+http.get(`${BASE_URL}/wanandroid`, {
     onData: (data) => {
         fill.fill(".wanandroid", data)
     }
 });
 
 fill.loading('.gityuan')
-http.get("http://127.0.0.1:9898/gityuan", {
+http.get(`${BASE_URL}/gityuan`, {
     onData: (data) => {
         fill.fill(".gityuan", data)
     }
